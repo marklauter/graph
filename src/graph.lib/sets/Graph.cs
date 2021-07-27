6173ac958fb7433b15e3ebd50a5173f94e922147
Mark Lauter <Mark.Lauter@insight.com>
2021-07-27 00:27:16 -0400

    added vertex map

--- src/graph.lib/sets/Graph.cs
+++ src/graph.lib/sets/Graph.cs
@@ -7,23 +7,20 @@ namespace graph.sets
     public class Graph
         : Element
     {
-        private Graph()
-        {
-            this.BuildVerticesMap();
-        }
+        private Graph() { }
 
         public Graph(string label)
             : base(label)
         {
-            this.Nodes = new Set<Node>($"{label} Nodes");
+            this.Vertices = new Set<Vertex>($"{label} Nodes");
             this.Edges = new Set<Edge>($"{label} Edges");
 
-            this.BuildVerticesMap();
+            this.map = new VertexMap(this.Edges);
         }
 
         public Graph(
             string label,
-            IEnumerable<Node> nodes,
+            IEnumerable<Vertex> nodes,
             IEnumerable<Edge> edges)
             : base(label)
         {
@@ -37,38 +34,15 @@ namespace graph.sets
                 throw new ArgumentNullException(nameof(edges));
             }
 
-            this.Nodes = new Set<Node>($"{label} Nodes", nodes);
+            this.Vertices = new Set<Vertex>($"{label} Nodes", nodes);
             this.Edges = new Set<Edge>($"{label} Edges", edges);
+            this.map = new VertexMap(this.Edges);
+        }        
 
-            this.BuildVerticesMap();
-        }
-
-        private readonly Dictionary<Guid, List<Guid>> vertices = new Dictionary<Guid, List<Guid>>();
-
-        public Set<Node> Nodes { get; }
+        public Set<Vertex> Vertices { get; }
 
         public Set<Edge> Edges { get; }
 
-        private void BuildVerticesMap()
-        {
-            foreach (var edge in this.Edges)
-            {
-                if (!this.vertices.TryGetValue(edge.V1.Id, out var v1Guids))
-                {
-                    v1Guids = new List<Guid>();
-                    this.vertices.Add(edge.V1.Id, v1Guids);
-                }
-
-                v1Guids.Add(edge.V2.Id);
-
-                if (!this.vertices.TryGetValue(edge.V2.Id, out var v2Guids))
-                {
-                    v2Guids = new List<Guid>();
-                    this.vertices.Add(edge.V2.Id, v2Guids);
-                }
-
-                v2Guids.Add(edge.V1.Id);
-            }
-        }
+        private readonly VertexMap map;
     }
 }
---
+++ src/graph.lib/sets/VertexMap.cs
@@ -0,0 +1,37 @@
+﻿using graph.elements;
+using System;
+using System.Collections.Generic;
+
+namespace graph.sets
+{
+    internal sealed class VertexMap
+    {
+        public VertexMap(Set<Edge> edges)
+        {
+            this.map = new Dictionary<Guid, List<Guid>>();
+
+            foreach (var edge in edges)
+            {
+                if (!this.map.TryGetValue(edge.Vertex1.Id, out var v1Guids))
+                {
+                    v1Guids = new List<Guid>();
+                    this.map.Add(edge.Vertex1.Id, v1Guids);
+                }
+
+                v1Guids.Add(edge.Vertex2.Id);
+
+                if (!this.map.TryGetValue(edge.Vertex2.Id, out var v2Guids))
+                {
+                    v2Guids = new List<Guid>();
+                    this.map.Add(edge.Vertex2.Id, v2Guids);
+                }
+
+                v2Guids.Add(edge.Vertex1.Id);
+            }
+        }
+
+        private readonly Dictionary<Guid, List<Guid>> map;
+
+        public List<Guid> this[Guid g] => this.map[g];
+    }
+}
