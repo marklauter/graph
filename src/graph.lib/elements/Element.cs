49044915a4c2fb3abbb7c92e554a495d137123e5
Mark Lauter <Mark.Lauter@insight.com>
2021-07-27 18:45:07 -0400

    experimenting with immutables

--- src/graph.lib/elements/Element.cs
+++ src/graph.lib/elements/Element.cs
@@ -1,30 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace graph.elements
 {
     public abstract class Element
     {
-        protected Element() { }
+        private readonly ImmutableSortedSet<string> classifications;
+        private readonly ImmutableDictionary<string, object> attributes;
 
-        protected Element(string Label)
+        protected Element()
         {
-            if (Label is null)
+            this.Id = Guid.NewGuid();
+            this.classifications = ImmutableSortedSet<string>.Empty;
+            this.attributes = ImmutableDictionary<string, object>.Empty;
+        }
+
+        protected Element(IEnumerable<string> classifications)
+            : this()
+        {
+            if (classifications is null)
             {
-                throw new ArgumentNullException(nameof(Label));
+                throw new ArgumentNullException(nameof(classifications));
             }
 
-            this.Label = Label;
-            this.Attributes = new Dictionary<string, object>();
-            this.Id = Guid.NewGuid();
+                this.classifications = this.classifications.Create<string>();
         }
 
         public Guid Id { get; }
 
-        public string Label { get; }
+        public IImmutableSet<string> Classifications => this.classifications;
 
-        public bool IsLabeled => !String.IsNullOrEmpty(this.Label);
+        public ImmutableDictionary<string, object> Attributes => this.attributes;
 
-        public Dictionary<string, object> Attributes { get; }
+        public string this[int index]
+        {
+            get => this.classifications[index];
+            set => this.classifications = this.classifications.Add
+        }
+
+        public object this[string key] => this.attributes[key];
+
+        public bool TrySetAttribute(string key, object value)
+        {
+            return this.attributes.TryGetValue(key, out var oldValue)
+                ? this.attributes.TryUpdate(key, value, oldValue)
+                : this.attributes.TryAdd(key, value);
+        }
+
+        public
+
+
     }
 }
