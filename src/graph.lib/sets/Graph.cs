bbc46c701ece2f3750ae7493e097710c422b6b5d
Mark Lauter <Mark.Lauter@insight.com>
2021-07-27 00:14:09 -0400

    added set and graph

---
+++ src/graph.lib/sets/Graph.cs
@@ -0,0 +1,74 @@
+﻿using graph.elements;
+using System;
+using System.Collections.Generic;
+
+namespace graph.sets
+{
+    public class Graph
+        : Element
+    {
+        private Graph()
+        {
+            this.BuildVerticesMap();
+        }
+
+        public Graph(string label)
+            : base(label)
+        {
+            this.Nodes = new Set<Node>($"{label} Nodes");
+            this.Edges = new Set<Edge>($"{label} Edges");
+
+            this.BuildVerticesMap();
+        }
+
+        public Graph(
+            string label,
+            IEnumerable<Node> nodes,
+            IEnumerable<Edge> edges)
+            : base(label)
+        {
+            if (nodes is null)
+            {
+                throw new ArgumentNullException(nameof(nodes));
+            }
+
+            if (edges is null)
+            {
+                throw new ArgumentNullException(nameof(edges));
+            }
+
+            this.Nodes = new Set<Node>($"{label} Nodes", nodes);
+            this.Edges = new Set<Edge>($"{label} Edges", edges);
+
+            this.BuildVerticesMap();
+        }
+
+        private readonly Dictionary<Guid, List<Guid>> vertices = new Dictionary<Guid, List<Guid>>();
+
+        public Set<Node> Nodes { get; }
+
+        public Set<Edge> Edges { get; }
+
+        private void BuildVerticesMap()
+        {
+            foreach (var edge in this.Edges)
+            {
+                if (!this.vertices.TryGetValue(edge.V1.Id, out var v1Guids))
+                {
+                    v1Guids = new List<Guid>();
+                    this.vertices.Add(edge.V1.Id, v1Guids);
+                }
+
+                v1Guids.Add(edge.V2.Id);
+
+                if (!this.vertices.TryGetValue(edge.V2.Id, out var v2Guids))
+                {
+                    v2Guids = new List<Guid>();
+                    this.vertices.Add(edge.V2.Id, v2Guids);
+                }
+
+                v2Guids.Add(edge.V1.Id);
+            }
+        }
+    }
+}
---
+++ src/graph.lib/sets/Set.cs
@@ -0,0 +1,46 @@
+﻿using graph.elements;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace graph.sets
+{
+    public class Set<T>
+        : Element, IEnumerable<T>
+        where T : Element
+    {
+        private Set() { }
+
+        public Set(string label)
+            : base(label)
+        {
+            this.Elements = new Dictionary<Guid, T>();
+        }
+
+        public Set(string label, IEnumerable<T> elements)
+            : this(label)
+        {
+            foreach (var element in elements)
+            {
+                this.Elements.Add(element.Id, element);
+            }
+        }
+
+        protected readonly Dictionary<Guid, T> Elements;
+
+        public T this[Guid g] => this.Elements[g];
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(var element in this.Elements.Values)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
