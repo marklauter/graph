ff2932d2de50b5a29494de342e84649372f476d7
Mark Lauter <Mark.Lauter@insight.com>
2021-07-28 01:40:40 -0400

    simplified graph

--- src/graph.lib/sets/Graph.cs
+++ src/graph.lib/sets/Graph.cs
@@ -1,57 +1,27 @@
-﻿using graph.elements;
+﻿using Graph.Elements;
 using System;
-using System.Collections.Generic;
 
-namespace graph.sets
+namespace Graph.Sets
 {
-    public class Graph
+    public sealed class Graph
         : Element
     {
-        private Graph() { }
+        private readonly EdgeIndex edgeIndex = EdgeIndex.Empty;
 
-        public Graph(IEnumerable<string> classifications)
-            : base(classifications)
+        public static Graph Empty => new();
+
+        public Set<Vertex> Vertices { get; } = Set<Vertex>.Empty;
+
+        public Set<Edge> Edges { get; } = Set<Edge>.Empty;
+
+        public int Degree(Guid vertexId)
         {
-            if (classifications is null)
-            {
-                throw new ArgumentNullException(nameof(classifications));
-            }
-
-            this.Vertices = Set<Vertex>.Empty;
-            this.Edges = Set<Edge>.Empty;
-            this.map = VertexMap.Empty;
+            return this.edgeIndex.Degree(vertexId);
         }
-
-        public Graph(
-            string label,
-            IEnumerable<Vertex> nodes,
-            IEnumerable<Edge> edges)
-            : base(label)
-        {
-            if (nodes is null)
-            {
-                throw new ArgumentNullException(nameof(nodes));
-            }
-
-            if (edges is null)
-            {
-                throw new ArgumentNullException(nameof(edges));
-            }
-
-            this.Vertices = new Set<Vertex>($"{label} Nodes", nodes);
-            this.Edges = new Set<Edge>($"{label} Edges", edges);
-            this.map = new VertexMap(this.Edges);
-        }        
-
-        public Set<Vertex> Vertices { get; }
-
-        public Set<Edge> Edges { get; }
-
-        private readonly VertexMap map;
 
         public int Degree(Vertex vertex)
         {
-            return this.map.Degree(vertex.Id);
+            return this.Degree(vertex.Id);
         }
     }
 }
