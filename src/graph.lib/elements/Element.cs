50db1653acc2b22467c807eab916abb95c87bb05
Mark Lauter <Mark.Lauter@insight.com>
2021-07-29 00:19:14 -0400

    added defensive argument checks

--- src/graph.lib/elements/Element.cs
+++ src/graph.lib/elements/Element.cs
@@ -20,25 +20,50 @@ namespace Graph.Elements
 
         public void Classify(string classification)
         {
-            this.classifications = this.classifications.Add(classification);
+            if (String.IsNullOrWhiteSpace(classification))
+            {
+                throw new ArgumentException($"'{nameof(classification)}' cannot be null or whitespace.", nameof(classification));
+            }
+
+            if (!this.classifications.Contains(classification))
+            {
+                this.classifications = this.classifications.Add(classification);
+            }
         }
 
         public void Classify(IEnumerable<string> classifications)
         {
+            if (classifications is null)
+            {
+                throw new ArgumentNullException(nameof(classifications));
+            }
+
             this.classifications = this.classifications
-                .Union(classifications)
+                .Union(classifications.Where(c => !String.IsNullOrWhiteSpace(c)))
+                .Distinct()
                 .ToImmutableList();
         }
 
         public void Qualify(string key, object value)
         {
+            if (String.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentException($"'{nameof(key)}' cannot be null or whitespace.", nameof(key));
+            }
+
             this.features = this.features.SetItem(key, value);
         }
 
         public void Qualify(IDictionary<string, object> features)
         {
+            if (features is null)
+            {
+                throw new ArgumentNullException(nameof(features));
+            }
+
             this.features = this.features
-                .Union(features)
+                .Union(features.Where(keyValue => !String.IsNullOrWhiteSpace(keyValue.Key)))
+                .Distinct()
                 .ToImmutableDictionary();
         }
     }
