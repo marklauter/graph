4070a7bdb44c58afee1ba2521a6763348fddb856
Mark Lauter <Mark.Lauter@insight.com>
2021-08-01 05:13:40 -0400

    added IGraph and NonDirectedAdjacencyMatrix

---
+++ src/graph.lib/Sets/IGraph.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Graph.Sets
+{
+    public interface IGraph<T>
+        : IEnumerable<T>
+    {
+        public void Connect(T vertex1, T vertex2);
+        public void Connect(T vertex1, T vertex2, byte weight);
+
+        public void Disconnect(T vertex1, T vertex2);
+
+        public int Degree(T vertex);
+
+        public bool Adjacent(T vertex1, T vertex2);
+
+        public IEnumerable<T> Neighbors(T vertex);
+
+        public IEnumerable<T> DepthFirstSearchPreOrder(T vertex);
+        
+        public IEnumerable<T> DepthFirstSearchPostOrder(T vertex);
+
+        public IEnumerable<T> BreadthFirstSearch(T vertex);
+
+        public int Size { get; }
+    }
+}
