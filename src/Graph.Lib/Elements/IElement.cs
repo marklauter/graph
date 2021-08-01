1b74afb9686f50425c25e6a9a494954d7e9e36c9
Mark Lauter <10211200+marklauter@users.noreply.github.com>
2021-08-01 18:57:38 -0400

    Feature/nodes n edges (#5)
    * added elements and storage fundamentals
    
    * renamed project
    
    * renamed project
    
    * added etag
    
    * changed namespace and added attributes to element
    
    * changed namespace
    
    * added set and graph
    
    * renamed node to vertex
    
    * added vertex map
    
    * added degree method
    
    * fixed youtube link
    
    * added references
    
    * moved to .net 5.0
    
    * experimenting with immutables
    
    * refactoring to immutability
    
    * added graph.io project
    
    * removed storage from graph project
    
    * added graph.io
    
    * simplified element class
    
    * simplified edge class
    
    * simplified vertex class
    
    * renamed vertexmap to edgeindex and simplified class
    
    * simplified graph
    
    * simplified set, made fluent, added explicit cast operators
    
    * sealed classes and white space
    
    * added test project
    
    * added defensive argument checks
    
    * renamed vertex test
    
    * working on sets
    
    * added IGraph and NonDirectedAdjacencyMatrix
    
    * added IElement and implemented IEquatable for Element
    
    * removed constructor
    
    * added some element tests
    
    * added tests for non directed adjaceny matrix
    
    * added image
    
    * added tests and fixed bugs
    
    * added tests
    
    * reimplementing the ragged index
    
    * refactored to IGraph and Graph base class for type independent algorithms
    
    * all tests succeed
    
    * removed path
    
    * removed weighted connect
    
    * removed Set{T} and KeyedSet{T}
    
    * added tests to get closer to 100% coverage
    
    * removed src to fix names
    
    * added src with correctly cased names
    
    Co-authored-by: Mark Lauter <Mark.Lauter@insight.com>

---
+++ src/Graph.Lib/Elements/IElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Graph.Elements
+{
+    public interface IElement
+    {
+        public Guid Id { get; }
+
+        public ImmutableHashSet<string> Labels { get; }
+        public void Classify(string label);
+        public void Classify(IEnumerable<string> labels);
+        public void Declassify(string label);
+        public bool Is(string label);
+
+        public IImmutableDictionary<string, object> Attributes { get; }
+        public void Qualify(string key, object value);
+        public void Qualify(IDictionary<string, object> attributes);
+        public object Attribute(string key);
+    }
+}
