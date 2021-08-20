bb55e64bbf4d699b52f5d6e82713d2d4fa8ec01a
Mark Lauter <Mark.Lauter@insight.com>
2021-08-20 00:38:26 -0400

    added breadth first traversal

---
+++ src/Graph.Traversals.Tests/BreadthFirstPreOrderTraversalTests.cs
@@ -0,0 +1,13 @@
+﻿using Graph.Indexes;
+
+namespace Graph.Traversals.Tests
+{
+    public sealed class BreadthFirstPreOrderTraversalTests
+        : TraversalTests
+    {
+        protected override ITraversal<int> CreateTraversal(IAdjacencyIndex<int> index)
+        {
+            return new BreadthFirstPreOrderTraversal<int>(index);
+        }
+    }
+}
