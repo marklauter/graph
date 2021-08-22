678ee6a0ef3dfd1e99853534423bc7c8cda94678
Mark Lauter <Mark.Lauter@insight.com>
2021-08-21 21:53:25 -0400

    fixing the command parser

--- src/Game.Controller/CommandParser.cs
+++ src/Game.Controller/CommandParser.cs
@@ -1,25 +1,19 @@
 ﻿using Game.Controller.Exceptions;
 using Graphs.Elements;
 using System;
-using System.Collections.Immutable;
 using System.Linq;
 
 namespace Game.Adventure
 {
     internal sealed class CommandParser
     {
-        private readonly ImmutableDictionary<string, Node> actions;
-        private readonly ImmutableDictionary<string, Node> objects;
+        private readonly IGraph graph;
+        private readonly Node player;
 
-        public CommandParser(IGraph graph)
+        public CommandParser(IGraph graph, Node player)
         {
-            this.actions = graph.Nodes
-                .Where(n => n.Is("action"))
-                .ToImmutableDictionary(a => a.Attribute("name"));
-
-            this.objects = graph.Nodes
-                .Where(n => n.Is("object"))
-                .ToImmutableDictionary(o => o.Attribute("name"));
+            this.graph = graph ?? throw new ArgumentNullException(nameof(graph));
+            this.player = player ?? throw new ArgumentNullException(nameof(player));
         }
 
         public Command Parse(string input)
@@ -43,6 +37,17 @@ namespace Game.Adventure
 
         private Node GetVerb(string[] parts, string input)
         {
+            var location = this.graph.Neighbors(this.player)
+                .Single(n => n.Is("location"));
+
+            var inventory = this.graph.Neighbors(this.player) // todo: make relationship queryable
+                .Where(n => n.Is("")); // todo: where relationship is inventory
+
+            var objects = this.graph.Neighbors(location)
+                .Union(inventory);
+
+            var actions = // todo: traverse a set of nodes 1 level deep to fetch the related actions - see the MIT video for frontier based BFS
+
             for (var i = 0; i < parts.Length; ++i)
             {
                 if (this.actions.TryGetValue(parts[i], out var verb))
